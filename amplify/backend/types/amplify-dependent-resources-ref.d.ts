402df5d5470701f780b2a5fd960fe3b69e1a6241
pepermao <joaopedrobatistasantos501@gmail.com>
2023-09-03 17:39:43 +0200

    Create payment with pagbank api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -4,9 +4,21 @@ export type AmplifyDependentResourcesAttributes = {
       "ApiId": "string",
       "ApiName": "string",
       "RootUrl": "string"
+    },
+    "PaymentAletheiaApi": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
     }
   },
   "function": {
+    "cardPaymentFunction": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    },
     "donateApi": {
       "Arn": "string",
       "LambdaExecutionRole": "string",
